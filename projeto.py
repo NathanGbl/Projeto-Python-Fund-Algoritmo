c6642d8153903cbcc2cabce54331beaba92b8d5d
NathanGbl <nathangabriel.f.leite@gmail.com>
2023-05-06 20:27:47 -0300

    Novas funções
    Adicionei o funcionamento das funções lista_clientes(), debito(), deposito() e transf_contas(). E comentários foram adicionados.

--- projeto.py
+++ projeto.py
@@ -1,48 +1,67 @@
-'''Essa função solicita os dados e adiciona estes em um dicionário
-e depois coloca esse dicionário na lista.'''
-def novo_cliente(): # Função que adiciona um cliente ao banco
-    nome = input("Nome: ")
-    cpf = input("CPF: ")
-    tipo_conta = input("Tipo de conta (Comum ou Plus?): ")
-    valor_inicial_conta = int(input("Valor inicial da conta: R$"))
-    senha_usuario = input("Senha: ")
-    dados_clientes = {}
-    dados_clientes['Nome'] = nome
+# Função que adiciona um cliente ao banco
+def novo_cliente():
+    nome = input('Nome: ').strip() # Linha 2, 3, 4 e 5 solicida dados
+    cpf = input('CPF: ').strip()
+    tipo_conta = input('Tipo de conta (Comum ou Plus?): ').capitalize().strip()
+    saldo = float(input('Valor inicial da conta: R$'))
+    senha_usuario = input('Senha: ')
+    dados_clientes = {} # Criação do dicionário
+    dados_clientes['Nome'] = nome # Linha 9, 10, 11, 12, 13 insere os dados no dicionário
     dados_clientes['CPF'] = cpf
     dados_clientes['Tipo de conta'] = tipo_conta
-    dados_clientes['Valor inicial da conta'] = valor_inicial_conta
+    dados_clientes['Saldo'] = saldo
     dados_clientes['Senha'] = senha_usuario
-    clientes.append(dados_clientes)
-    
-'''Essa função itera em cada chave CPF comparando o cpf digitado com o que existe na chave.
-Se estiver, exclui. Senão, pergunta novamente o cpf para apagar.'''
-def apagar_cliente(): # Função que retira algum cliente do banco
-    cpf_apagar = input('Digite o cpf para apagar a conta: ')
-    for indice in range(len(clientes)):
+    clientes.append(dados_clientes) # Adiciona o dicionário como um elemento da lista clientes
+
+# Função que retira algum cliente do banco
+def apagar_cliente():
+    cpf_apagar = input('Digite o cpf para apagar a conta: ') # Solicida o cpf que será apagado
+    for indice in range(len(clientes)): # Laço itera sobre cada chave 'CPF' dos dicionários e, se achar o cpf, deleta, senão, avisa que o cpf não foi encontrado.
         if cpf_apagar == clientes[indice]['CPF']:
             del clientes[indice]
             break
     else:
         print(f'CPF {cpf_apagar} não encontrado. Tente novamente mais tarde.')
 
-'''Essa função itera sobre cada dicionário na lista clientes e printa o valor que está
-na chave Nome.'''
-def lista_clientes(): # Função que mostra a lista com clientes
+# Função que mostra a lista com clientes
+def lista_clientes():
     print('Lista de clientes:')
-    for indice in range(len(clientes)):
-        print(clientes[indice]['Nome'])
-    
-def debito(): # Função que debita um valor
-    cpf_debito = float(input("Digite o CPF da sua conta: "))
+    for indice in range(len(clientes)): # itera sobre cada dicionário na lista cliente e imprimi o conteúdo do dicionário.
+        print(clientes[indice])
+
+# Função que debita um valor            
+def debito():
+    cpf_debito = input('Digite o CPF da sua conta: ') # Linha 34, 35, 36 solicita dados
     senha_debito = input('Digite sua senha: ')
     valor_debito = float(input('Digite o valor a ser debitado: '))
+    for indice in range(len(clientes)): # Itera sobre cada dicionário na lista cliente e verifica os dados recebidos.
+        if cpf_debito == clientes[indice]['CPF'] and senha_debito == clientes[indice]['Senha'] and clientes[indice]['Tipo de conta'] == 'Comum':
+            clientes[indice]['Saldo'] -= valor_debito * 1.05
+            if clientes[indice]['Saldo'] < -1000: # Se os dados estiverem corretos e o saldo ficar 1000 negativo, o valor é devolvido e o débito é cancelado.
+                clientes[indice]['Saldo'] += valor_debito * 1.05
+                print('Transação ultrapassou o limite de saldo negativo. Transação cancelada!')
+            elif clientes[indice]['Saldo'] >= -1000: # Se os dados estiverem corretos e o saldo ficar acima de 1000 negativo, o débito é realizado
+                print(f'Transação realizada com sucesso!\nSaldo: {clientes[indice]["Saldo"]}')
+        elif cpf_debito == clientes[indice]['CPF'] and senha_debito == clientes[indice]['Senha'] and clientes[indice]['Tipo de conta'] == 'Plus':
+            clientes[indice]['Saldo'] -= valor_debito * 1.03
+            if clientes[indice]['Saldo'] < -5000:
+                clientes[indice]['Saldo'] += valor_debito * 1.03
+                print('Trasação ultrapassou o limite de saldo negativo. Transação cancelada!')
+            elif clientes[indice]['Saldo'] >= -5000:
+                print(f'Transação realizada com sucesso!\nSaldo: {clientes[indice]["Saldo"]}')
 
-def deposito(): # Função que adiciona um valor na conta do usuario 
-    cpf_deposito = float(input("CPF que você deseja enviar: "))
-    senha_deposito = input("Senha: ")
-    valor_deposito = float(input("Valor que irá depositar: "))
+# Função que adiciona um valor na conta do usuario 
+def deposito():
+    cpf_deposito = input('CPF que você deseja enviar: ')
+    senha_deposito = input('Senha: ')
+    valor_deposito = float(input('Valor que irá depositar: '))
+    for indice in range(len(clientes)): # Itera sobre cada dicionário na lista cliente e verifica os dados recebidos.
+        if cpf_deposito == clientes[indice]['CPF'] and senha_deposito == clientes[indice]['Senha']:
+            clientes[indice]['Saldo'] += valor_deposito # Se os dados estiverem corretos, o valor é adicionado à conta e o saldo é imprimido na tela.
+            print(f'Saldo: {clientes[indice]["Saldo"]:.2f}')
 
-def extrato(): # Função que mostra o extrato da conta bancaria do usuario
+# Função que mostra o extrato da conta bancaria do usuario
+def extrato():
     cpf_extrato = float(input('Digite o cpf para retirar o extrato: '))
     senhaextrato = input('Digite a senha: ')
     print(f'Nome: {nome}')
@@ -50,14 +69,34 @@ def extrato(): # Função que mostra o extrato da conta bancaria do usuario
     print(f'Conta: {tipo_conta}')
     print(Transferencias)
 
-def tranf_contas(): # Função que faz transferencias 
-    cpf_transferencia = float(input("Digite o cpf para realizar a transferencia: "))
-    senha_transferencia = input('Digite sua senha: ')
-    cpf_destino_transferencia = float(input('Digite o cpf do destinatário: '))
-    valor_transferecia = float(input('Valor da tranferência: '))
-
+# Função que faz transferencias 
+def transf_contas():
+    cpf_origem = input('Digite o CPF da sua conta: ') # Linha 74, 75, 76 e 77 solicita dados.
+    senha_origem = input('Digite sua senha: ')
+    cpf_final = input('Digite o CPF do destinatário: ')
+    valor_transferencia = float(input('Digite o valor a ser debitado: '))
+    print()
+    for indice in range(len(clientes)): # Verifica se o cpf de origem existe no sistema.
+        if cpf_origem == clientes[indice]['CPF'] and senha_origem == clientes[indice]['Senha']:
+            break
+    else:
+        print('Dados incorretos.')
+    for indice in range(len(clientes)): # Verifica se o cpf final exsite no sistema.
+        if cpf_final == clientes[indice]['CPF']:
+            break
+    else:
+        print('Dados incorretos. Tente novamente mais tarde.')
+    for indice in range(len(clientes)): # Realiza a retirada do valor da conta do cpf de origem.
+        if cpf_origem == clientes[indice]['CPF'] and senha_origem == clientes[indice]['Senha']:
+            clientes[indice]['Saldo'] -= valor_transferencia
+            print(f'Transferência realizada com sucesso!\nSaldo: {clientes[indice]["Saldo"]:.2f}')
+            break
+    for indice in range(len(clientes)): # Adiciona o valor à conta do cpf final.
+        if cpf_final == clientes[indice]['CPF']:
+            clientes[indice]['Saldo'] += valor_transferencia
+            break
 def invest(): # Função que faz operações livres 
-    tipo_invest = input("Qual tipo de invstimento você deseja fazer??")
+    tipo_invest = input('Qual tipo de invstimento você deseja fazer?')
     investimento = float(input('Valor a investir: '))
     tempo_investimento = int(input('Tempo de investimeto em meses: '))
 
@@ -79,23 +118,21 @@ while True:
     print()
     if entrada == 9:
         print("Muito obrigado por ter escolhido nosso banco!")
-        break
-    elif entrada == 1:
+    elif entrada == 1: # Chama a função novo_cliente()
         novo_cliente()
         print(clientes)
-    elif entrada == 2:
+    elif entrada == 2: # Chama a função apagar_cliente()
         apagar_cliente()
         print(clientes)
-    elif entrada == 3:
+    elif entrada == 3: # Chama a função lsita_clientes()
         lista_clientes()
-    elif entrada == 4:
-        print(debito())
-    elif entrada == 5:
-        print(deposito())
-    elif entrada == 6:
-        print(extrato())
-    elif entrada == 7:
-        print(tranf_contas())
-    elif entrada == 8:
-        print(invest()) 
-
+    elif entrada == 4: # Chama a função debito()
+        debito()
+    elif entrada == 5: # Chama a função deposito()
+        deposito()
+    elif entrada == 6: # Chama a função extrato()
+        extrato()
+    elif entrada == 7: # Chama a função tranf_contas()
+        transf_contas()
+    elif entrada == 8: # Chama a função invest()
+        invest()
